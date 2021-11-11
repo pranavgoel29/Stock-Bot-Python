b75a01ed719fc6fb74f1bbfd39a288a88ac60d65
Mudit Mishra <Mudit.12016298@lpu.in>
2021-11-11 10:32:34 +0530

    Added beta stock feature

--- main.py
+++ main.py
@@ -22,6 +22,32 @@ def get_quote():
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return(quote)
 
+def get_stock():
+  response = ""
+  stocks = ['gme', 'amc', 'nok']
+  stock_data = []
+  for stock in stocks:
+    data = yf.download(tickers=stock, period='2d', interval='1d')
+    response += f"--------{stock}--------\n"
+    data = data.reset_index()
+    response += f"-----{stock}-----\n"
+    stock_data.append([stock])
+    columns = ['stock']
+    for index, row in data.iterrows():
+      stock_position = len(stock_data) - 1
+      price = round(row['Close'], 2)
+      format_date = row['Date'].strftime('%m/%d')
+      response += f"{format_date}: {price}\n"
+      stock_data[stock_position].append(price)
+      columns.append(format_date)
+    print()
+
+  response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
+  for row in stock_data:
+    response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
+  response += "\nStock Data"
+  return(response)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -36,6 +62,9 @@ async def on_message(message):
     if msg.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+    if msg.startswith('!stock'):
+        stock_data = get_stock()
+        await message.channel.send(stock_data)
  
     if msg.startswith('Greet') or msg.startswith('greet'):
         await message.channel.send('Hey! Hows it going?')
@@ -43,65 +72,3 @@ async def on_message(message):
 
 client.run(os.getenv('TOKEN'))
 
-# Stock part
-
-# API_KEY = os.getenv('TOKEN')
-# bot = client.run(API_KEY)
-
-
-
-
-# def greet(message):
-
-# @bot.message_handler(commands=['hello'])
-# def hello(message):
-#   bot.send_message(message.chat.id, "Hello!")
-
-# @bot.message_handler(commands=['wsb'])
-# def get_stocks(message):
-#   response = ""
-#   stocks = ['gme', 'amc', 'nok']
-#   stock_data = []
-#   for stock in stocks:
-#     data = yf.download(tickers=stock, period='2d', interval='1d')
-#     data = data.reset_index()
-#     response += f"-----{stock}-----\n"
-#     stock_data.append([stock])
-#     columns = ['stock']
-#     for index, row in data.iterrows():
-#       stock_position = len(stock_data) - 1
-#       price = round(row['Close'], 2)
-#       format_date = row['Date'].strftime('%m/%d')
-#       response += f"{format_date}: {price}\n"
-#       stock_data[stock_position].append(price)
-#       columns.append(format_date)
-#     print()
-
-#   response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
-#   for row in stock_data:
-#     response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
-#   response += "\nStock Data"
-#   print(response)
-#   bot.send_message(message.chat.id, response)
-
-# def stock_request(message):
-#   request = message.text.split()
-#   if len(request) < 2 or request[0].lower() not in "price":
-#     return False
-#   else:
-#     return True
-
-# @bot.message_handler(func=stock_request)
-# def send_price(message):
-#   request = message.text.split()[1]
-#   data = yf.download(tickers=request, period='5m', interval='1m')
-#   if data.size > 0:
-#     data = data.reset_index()
-#     data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
-#     data.set_index('format_date', inplace=True)
-#     print(data.to_string())
-#     bot.send_message(message.chat.id, data['Close'].to_string(header=False))
-#   else:
-#     bot.send_message(message.chat.id, "No data!?")
-
-# bot.polling()
