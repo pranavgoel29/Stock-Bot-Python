e17f8ea0161bac8a822bb9b1c2b0ab48563bd182
pranavgoel29 <pranavgoel29@gmail.com>
2021-11-15 01:34:17 +0530

    All Functions are working including the custom stock from chat one.

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ def get_quote():
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return(quote)
 
+# Function for custom stock
+
+def send_price(pricec):
+    # request = message.content.split()[1]
+  request = pricec
+  data = yf.download(tickers=request, period='5m', interval='1m')
+  if data.size > 0:
+    data = data.reset_index()
+    data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
+    data.set_index('format_date', inplace=True)
+    print(data.to_string())
+    return(data['Close'].to_string(header=False))
+  else:
+    return("No data!?")
+
+
+# Function for some predefined Stocks.
+
 def get_stock():
   response = ""
   stocks = ['gme', 'amc', 'nok']
@@ -46,12 +64,18 @@ def get_stock():
   for row in stock_data:
     response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
   response += "\nStock Data"
-  return(response)
+  return(string(response))
+
+
+# Server login message function.
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     
+
+# Function for commands from message of Discord.
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -59,9 +83,11 @@ async def on_message(message):
 
     msg = message.content
 
+
     if msg.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+
     if msg.startswith('!stock'):
         stock_data = get_stock()
         await message.channel.send(stock_data)
@@ -70,5 +96,13 @@ async def on_message(message):
         await message.channel.send('Hey! Hows it going?')
 
 
-client.run(os.getenv('TOKEN'))
+    if msg.startswith('!price'):
+    # request = message.content.split()
+    # if len(request) < 2 or request[0].lower() not in "!price":
+    #   return False
+    # else:
+      pricec = msg.split()[1]
+      price_custom = send_price(pricec)
+      await message.channel.send(price_custom)
 
+client.run(os.getenv('TOKEN'))
